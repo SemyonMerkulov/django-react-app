e4792efa45fefe9789aa4935a1da89c8e9d0e9c0
SemyonMerkulov <semen.merkulov@gmail.com>
2021-04-21 18:16:52 +0700

    add post app

--- api/api/urls.py
+++ api/api/urls.py
@@ -18,6 +18,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('admin/', admin.site.urls)
 ]
